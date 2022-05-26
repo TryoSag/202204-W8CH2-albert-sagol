de9f928e09ef02a764ed3712b277c07b413f2b0f
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-26 20:32:55 +0200

    robotos again the remake

--- src/types/Robot.ts
+++ src/types/Robot.ts
@@ -2,15 +2,18 @@ type Type = "Cleaner" | "Waiter" | "Developer";
 
 interface IRobot {
   name: string;
-  type: Type;
   batteryLevel: number;
+  type: Type;
   reset: () => void;
   work: () => void;
 }
-
 class Robot implements IRobot {
   batteryLevel;
-  constructor(public name: string, public type: Type, private message: string) {
+  constructor(
+    public name: string,
+    protected type: Type,
+    protected message: string
+  ) {
     this.batteryLevel = 100;
   }
   reset() {}
@@ -18,4 +21,22 @@ class Robot implements IRobot {
     console.log(this.message);
   }
 }
-export default Robot;
+export class Cleaner extends Robot {
+  constructor(public name: string) {
+    super(name, "Cleaner", "Larala larita, I clean my little house");
+  }
+}
+export class Witer extends Robot {
+  constructor(public name: string) {
+    super(name, "Waiter", "Do you feel like a mini of fuet?");
+  }
+}
+export class Developer extends Robot {
+  constructor(public name: string) {
+    super(
+      name,
+      "Developer",
+      "JavaScript is cool - I develop with JavaScript -> I'm cool"
+    );
+  }
+}
